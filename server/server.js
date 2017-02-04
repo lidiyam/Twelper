bcbda1d2d85f48f2d2e828335c1a34dc9e1590fa
Aaron Persaud <aaronpersaud@hotmail.com>
2017-02-04 03:46:23 -0500

    added yelp API stuff and sample

--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var yelpCreds = require('./yelpSecret');
+var yelp = require('yelp-fusion');
+
+var clientId = yelpCreds.clientId;
+var clientSecret = yelpCreds.clientSecret;
 
 app.use(bodyParser())
+
+//test example
+var searchRequest = {
+	//why is this filter not working?????
+	category_filter: 'tours',
+	location: 'san francisco, ca',
+	limit:10
+};
+
+yelp.accessToken(clientId, clientSecret).then(response => {
+  var client = yelp.client(response.jsonBody.access_token);
+
+  client.search(searchRequest).then(response => {
+    var Result = response.jsonBody.businesses;
+    var prettyJson = JSON.stringify(Result, null, 4);
+    console.log(prettyJson);
+  });
+}).catch(e => {
+  console.log(e);
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
