dac3c1b1bc89d3fa185ddb03d5f4bc264d92e519
Lidiya Murakhovs'ka <lidiya.murakhovskaya@gmail.com>
2017-02-04 05:15:47 -0500

    uber api set up

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var app = express()
 var bodyParser = require('body-parser')
 var yelpCreds = require('./yelpSecret');
 var yelp = require('yelp-fusion');
+var uberCreds = require('./uberSecret');
+var Uber = require('node-uber');
 
 var clientId = yelpCreds.clientId;
 var clientSecret = yelpCreds.clientSecret;
@@ -29,6 +31,21 @@ yelp.accessToken(clientId, clientSecret).then(response => {
   console.log(e);
 });
 
+var uber = new Uber({
+  client_id: uberCreds.clientIdUber,
+  client_secret: uberCreds.clientSecretUber,
+  server_token: uberCreds.serverToken,
+  redirect_uri: 'http://localhost:8000/api/callback',
+  name: 'Twelper',
+  language: 'en_US', // optional, defaults to en_US
+  sandbox: true // optional, defaults to false
+});
+
+uber.estimates.getPriceForRoute(3.1357, 101.6880, 3.0833, 101.6500, function (err, res) {
+  if (err) console.error(err);
+  else console.log(res);
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -36,3 +53,4 @@ app.get('/', function (req, res) {
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
 })
+
